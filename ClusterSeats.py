3f2f55ade38cda3d4af9dcb8ed894988a6324a6f
Clinton Boys <cdsboys@gmail.com>
2015-04-28 21:49:15 +0300

    Updated clustering

--- ClusterSeats.py
+++ ClusterSeats.py
@@ -26,54 +26,33 @@ census_data.columns = ['divisions', 'Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var
 
 print census_data.head()
 
-'''
+kmeans = KMeans(2)
+kmeans.fit(census_data[['Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var6', 'Var7', 'Var8', 'Var9']])
 
-census <- read.csv('2006_census_data_by_division.csv')
-summary(census)
-pop <- as.numeric(as.character(census$Total_population[1:150]))
-Var1 <- as.numeric(as.character(census$Labour_force_aged_45_years_and_over22[1:150]))/pop
-Mig1 <- as.numeric(as.character(census$Overseas[1:150]))
-Mig2 <- as.numeric(as.character(census$NonEngBorn.[1:150]))
-Mig3 <- as.numeric(as.character(census$Persons_who_speak_English_not_well_or_not_at_all[1:150]))/pop
-Mig4 <- as.numeric(as.character(census$Persons_speaking_a_language_other_than_English_at_home[1:150]))/pop
-Var2 <- (Mig1+Mig2+Mig3+Mig4)/4
-Var3 <- as.numeric(as.character(census$Persons_employed_in_agriculture[1:150]))/pop
-Var4 <- as.numeric(as.character(census$Unemployed_persons[1:150]))/pop
-Var5 <- as.numeric(as.character(census$Median_age[1:150]))
-Var6 <- as.numeric(as.character(census$Total_dependency_ratio3[1:150]))
-Var7 <- as.numeric(as.character(census$Persons_aged_15_to_24_years[1:150]))/pop
-Var8 <- as.numeric(as.character(census$Catholic.[1:150]))
-Var9 <- log(as.numeric(as.character(census$Population_density1[1:150])))
-divs <- census$division[1:150]
-df = data.frame(divs, Var1, Var2, Var3, Var4, Var5, Var6, Var7, Var8, Var9)
+census_data['2clusters'] = kmeans.labels_
 
-two_clusters <- kmeans(df[,2:10],2)
-df$divs[two_clusters$cluster==1]
-df$divs[two_clusters$cluster==2]
+kmeans = KMeans(3)
+kmeans.fit(census_data[['Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var6', 'Var7', 'Var8', 'Var9']])
 
-three_clusters <- kmeans(df[,2:9],3)
-df$divs[three_clusters$cluster==1]
-df$divs[three_clusters$cluster==2]
-df$divs[three_clusters$cluster==3]
+census_data['3clusters'] = kmeans.labels_
 
-ten_clusters <- kmeans(df[,2:10],10)
-df$divs[ten_clusters$cluster==1]
-df$divs[ten_clusters$cluster==2]
-df$divs[ten_clusters$cluster==3]
-df$divs[ten_clusters$cluster==4]
-df$divs[ten_clusters$cluster==5]
-df$divs[ten_clusters$cluster==6]
-df$divs[ten_clusters$cluster==7]
-df$divs[ten_clusters$cluster==8]
-df$divs[ten_clusters$cluster==9]
-df$divs[ten_clusters$cluster==10]
+kmeans = KMeans(6)
+kmeans.fit(census_data[['Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var6', 'Var7', 'Var8', 'Var9']])
 
-six_clusters <- kmeans(df[,2:10],6)
-six_clusters
-df$divs[six_clusters$cluster==1]
-df$divs[six_clusters$cluster==2]
-df$divs[six_clusters$cluster==3]
-df$divs[six_clusters$cluster==4]
-df$divs[six_clusters$cluster==5]
-df$divs[six_clusters$cluster==6]
-'''
+census_data['6clusters'] = kmeans.labels_
+
+kmeans = KMeans(10)
+kmeans.fit(census_data[['Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var6', 'Var7', 'Var8', 'Var9']])
+
+census_data['10clusters'] = kmeans.labels_
+
+print census_data.divisions[census_data['6clusters'] == 0]
+print census_data.divisions[census_data['6clusters'] == 1]
+print census_data.divisions[census_data['6clusters'] == 2]
+print census_data.divisions[census_data['6clusters'] == 3]
+print census_data.divisions[census_data['6clusters'] == 4]
+print census_data.divisions[census_data['6clusters'] == 5]
+
+
+
+
