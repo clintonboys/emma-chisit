c9ce72305034b52d24099553fd14a67c26b6b285
EC2 Default User <ec2-user@ip-172-31-34-184.us-west-2.compute.internal>
2016-02-05 16:40:06 +0000

    Tweet scraper now cronned daily

--- data/ghost_who_votes/AutomaticScraper.py
+++ data/ghost_who_votes/AutomaticScraper.py
@@ -112,7 +112,8 @@ def main():
 		since_id = data['tweet_id'].iloc[0]
 	else: 
 		since_id = 550642284887547904  # First tweet of 2015
-	start_time = time.time()
+	print since_id
+        start_time = time.time()
 
 	max_id   = 999999999999999999
 
